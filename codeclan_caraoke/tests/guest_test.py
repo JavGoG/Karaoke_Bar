b2609aeebf3b478dd49c6617eaffa6489bdfca75
JavGog <javierggjavier@gmail.com>
2022-07-03 17:03:49 -0700

    tests passed from class TestGuess

--- codeclan_caraoke/tests/guest_test.py
+++ codeclan_caraoke/tests/guest_test.py
@@ -4,3 +4,9 @@ import unittest
 class TestGuest(unittest.TestCase):
     def setUp(self):
         self.guest1 = Guest("Michael", 100)
+
+    def test_name_of_guest(self):
+        self.assertEqual("Michael", self.guest1.name)
+
+    def test_wallet_of_guest(self):
+        self.assertEqual(100, self.guest1.wallet)
